5abffe370457c76440782f32f119b8c08c71ac04
siunlucky <74404911+siunlucky@users.noreply.github.com>
2022-01-19 07:22:30 +0700

    Add files via upload

---
+++ tugas1/soal2.js
@@ -0,0 +1,10 @@
+let barang = [
+    { nama : "Beras", harga : 10000, jumlah : 5},
+    { nama : "Gula", harga : 14000, jumlah : 5},
+    { nama : "Telur", harga : 20000, jumlah : 2},
+    { nama : "Minyak Goreng", harga : 9000, jumlah : 10},
+]
+
+for (let i = 0; i < barang.length; i++) {
+    let total_harga    
+}
