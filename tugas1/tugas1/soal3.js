566c34c0a6a32d209b0ee00b59fa560ab7357d36
siunlucky <74404911+siunlucky@users.noreply.github.com>
2022-01-19 08:34:31 +0700

    Add files via upload

---
+++ tugas1/tugas1/soal3.js
@@ -0,0 +1,27 @@
+let data = {
+    nama : "Rhodey",
+    tinggi_badan : 1.7,
+    berat_badan : 90
+}
+let bmi = data.berat_badan / Math.pow(data.tinggi_badan, 2);
+console.log("Nama anda = " + data.nama);
+console.log("Berat Badan anda = " + data.berat_badan);
+console.log("Tinggi Badan anda = " + data.tinggi_badan);
+console.log("BMI anda = " + bmi);
+
+if (bmi < 18.5) {
+    console.log("Anda Kekurangan Berat Badan");
+        
+}
+if (bmi >= 18.5 && bmi <= 24.9) {
+    console.log("Anda Normal");
+        
+}
+if (bmi >= 25 && bmi < 30) {
+    console.log(" Anda Kelebihan Berat Badan");
+        
+}
+if (bmi >= 30 ) {
+    console.log("Anda Obesitas");
+        
+}
